6e71a7ab06cd2253ed259749e37da2cc27ec73ab
xpart <abullaalbinroni@gmail.com>
2026-08-21 12:19:45 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+function register() {
+
+let name = prompt("Enter your name")
+let gmail = prompt("Enter your Gmail")
+let age = Number(prompt("Enter your age"))
+
+    checkAge(age, name)
+}
+
+
+function login() {
+
+let name = prompt("Enter your name")
+let age = Number(prompt("Enter your age"))
+
+    checkAge(age, name);
+}
+
+
+function checkAge(age, name) {
+
+if (age < 18) {
+alert("You can't enter. You are a student.")
+
+} else if (age < 30) {
+
+ alert("You are not a teacher.")
+
+} else if (age <= 80) {
+
+    document.getElementById("login").style.display = "none"
+document.getElementById("website").style.display = "block"
+
+document.getElementById("welcomeMessage").innerText =
+    "Welcome " + name + "!"
+
+} else {
+
+        alert("A horror incident happened... 👻")
+
+    }
+}
