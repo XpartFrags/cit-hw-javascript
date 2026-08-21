ffdaa66c49c7c9430a8afc8932cc3554d1487fdb
xpart <abullaalbinroni@gmail.com>
2026-08-21 14:24:08 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function register() {
 
-let name = prompt("Enter your name")
-let gmail = prompt("Enter your Gmail")
-let age = Number(prompt("Enter your age"))
+    let name = prompt("Enter your name");
+    let gmail = prompt("Enter your Gmail");
+    let age = Number(prompt("Enter your age"));
 
-    checkAge(age, name)
+    checkAge(age, name);
 }
 
 
 function login() {
 
-let name = prompt("Enter your name")
-let age = Number(prompt("Enter your age"))
+    let name = prompt("Enter your name");
+    let age = Number(prompt("Enter your age"));
 
     checkAge(age, name);
 }
@@ -19,33 +19,25 @@ let age = Number(prompt("Enter your age"))
 
 function checkAge(age, name) {
 
-if (age < 18) {
-alert("You can't enter. You are a student.")
+    if (age < 18) {
 
-}  else if (age < 30) {
-    alert("Keep learning!");
+        alert("You can't enter. You are a student.");
 
-} else if (age <= 80) {
-    // Enter the website
+    } else if (age < 30) {
 
-} else {
-    alert("A horror incident happened... 👻");
-}
-else if (age < 30) {
+        alert("You are not a teacher.");
 
- alert("You are not a teacher.")
+    } else if (age <= 80) {
 
-} else if (age <= 80) {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("website").style.display = "block";
 
-    document.getElementById("login").style.display = "none"
-document.getElementById("website").style.display = "block"
+        document.getElementById("welcomeMessage").innerText =
+            "Welcome " + name + "!";
 
-document.getElementById("welcomeMessage").innerText =
-    "Welcome " + name + "!"
+    } else {
 
-} else {
-
-        alert("A horror incident happened... 👻")
+        alert("A horror incident happened... 👻");
 
     }
 }
